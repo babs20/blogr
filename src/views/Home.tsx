e78e5cf7da534b2c9e5a68bb176b67a00f0b309b
babs20 <thevillemc@gmail.com>
2021-05-28 00:42:27 -0400

    add homepage view

---
+++ src/views/Home.tsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import InfoArticle from '../components/InfoArticle';
+
+export const Home = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <main>
+        <InfoArticle />
+      </main>
+    </>
+  );
+};
+
+export default Home;
