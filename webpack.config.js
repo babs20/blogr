ea7269f947e95ef52760ad41a83e2461dfcb4109
babs20 <thevillemc@gmail.com>
2021-05-29 01:41:29 -0400

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// 1. import default from the plugin module
+const createStyledComponentsTransformer =
+  require('typescript-plugin-styled-components').default;
+
+// 2. create a transformer;
+// the factory additionally accepts an options object which described below
+const styledComponentsTransformer = createStyledComponentsTransformer();
+
+// 3. add getCustomTransformer method to the loader config
+const config = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        options: {
+          getCustomTransformers: () => ({
+            before: [styledComponentsTransformer],
+          }),
+        },
+      },
+    ],
+  },
+};
